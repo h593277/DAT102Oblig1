1cb745f3fba17961f1acf682f8692265ea541166
Markus Løtveit <68389075+EmperorNOR@users.noreply.github.com>
2021-01-19 11:29:19 +0100

    FilmArkivet
    Implementasjon av FilmArkiv klassen.

--- src/no/hvl/data102/Film.java
+++ src/no/hvl/data102/Film.java
@@ -7,20 +7,67 @@ public class Film {
 	private String tittel;
 	private int år;
 	private String filmSelskap;
-	private enum sjanger {SKREKK, ACTION, ROMANTISK, KOMEDIE, DOKUMENTAR, THRILLER, FANTASY}
+	Sjanger sjangeren;
+	
+	public int getFilmnr() {
+		return filmnr;
+	}
+
+	public void setFilmnr(int filmnr) {
+		this.filmnr = filmnr;
+	}
+
+	public String getFilmskaper() {
+		return filmskaper;
+	}
+
+	public void setFilmskaper(String filmskaper) {
+		this.filmskaper = filmskaper;
+	}
+
+	public String getTittel() {
+		return tittel;
+	}
+
+	public void setTittel(String tittel) {
+		this.tittel = tittel;
+	}
+
+	public int getÅr() {
+		return år;
+	}
+
+	public void setÅr(int år) {
+		this.år = år;
+	}
+
+	public String getFilmSelskap() {
+		return filmSelskap;
+	}
+
+	public void setFilmSelskap(String filmSelskap) {
+		this.filmSelskap = filmSelskap;
+	}
 	
 	public Film() 
 	{
 		
 	}
 	
-	public Film(int filmnr, String filmskaper, String tittel, int år, String filmSelskap) 
+	public Film(int filmnr, String filmskaper, String tittel, int år, String filmSelskap, String Sjangeren) 
 	{
 		this.filmnr = filmnr;
 		this.filmskaper = filmskaper;
 		this.tittel = tittel;
 		this.år = år;
 		this.filmSelskap = filmSelskap;
+		sjangeren = sjangeren.valueOf(Sjangeren);
+	}
+
+	@Override
+	public String toString() {
+		return "Film [filmnr=" + filmnr + ", filmskaper=" + filmskaper + ", tittel=" + tittel + ", år=" + år
+				+ ", filmSelskap=" + filmSelskap + ", sjangeren=" + sjangeren + "]";
 	}
 	
 	
--- src/no/hvl/data102/FilmArkiv.java
+++ src/no/hvl/data102/FilmArkiv.java
@@ -5,6 +5,7 @@ import no.hvl.data102.adt.FilmArkivADT;
 public class FilmArkiv implements FilmArkivADT {
 
 	Film[] filmArkivet;
+	int antall = 0;
 	public FilmArkiv(int n)
 	{
 	  filmArkivet = new Film[n];
@@ -12,32 +13,76 @@ public class FilmArkiv implements FilmArkivADT {
 	
 	@Override
 	public boolean LeggTil(Film filmen) {
-		// TODO Auto-generated method stub
-		return false;
+		
+		boolean lagtTil = false;
+		
+		if(antall < filmArkivet.length)
+		{
+			filmArkivet[antall] = filmen;
+			antall++;
+			lagtTil = true;
+		}
+		else
+		{
+			Film[] hjelpeTabell = new Film[filmArkivet.length*2];
+			for(int i = 0; i < filmArkivet.length; i++)
+			{
+				hjelpeTabell[i] = filmArkivet[i];
+			}
+			
+			filmArkivet = hjelpeTabell;
+			filmArkivet[antall] = filmen;
+			antall++;
+			lagtTil = true;
+		}
+		return lagtTil;
 	}
 
 	@Override
 	public boolean SlettFilm(Film filmen) {
-		// TODO Auto-generated method stub
-		return false;
+		boolean slettet = false;
+		
+		for(int i = 0; i < filmArkivet.length; i++)
+		{
+			if(filmArkivet[i].equals(filmen))
+			{
+				filmArkivet[i] = filmArkivet[antall];
+				filmArkivet[antall] = null;
+				antall--;
+				slettet = true;
+			}
+		}
+		return slettet;
 	}
 
 	@Override
 	public Film FinnFilm(String tittel) {
-		// TODO Auto-generated method stub
+		for(Film finn : filmArkivet)
+		{
+			if(finn.getTittel().contains(tittel))
+			{
+				return finn;
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public String FinnProdusent(String produsent) {
-		// TODO Auto-generated method stub
+		for(Film finn : filmArkivet)
+		{
+			if(finn.getTittel().contains(produsent))
+			{
+				return finn.getFilmskaper();
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public int AntallFilmer() {
-		// TODO Auto-generated method stub
-		return 0;
+
+		return filmArkivet.length-1;
 	}
 
 }
