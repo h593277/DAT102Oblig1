c7e071394c4472acd9739ff206b1e0242f5a3502
Mads Olsen <589710@hvl.no>
2021-02-09 18:25:26 +0100

    ny metode i tekstgrensesnitt

--- src/no/hvl/data102/Sjanger.java
+++ src/no/hvl/data102/Sjanger.java
@@ -1,10 +1,7 @@
 package no.hvl.data102;
 
 public enum Sjanger {
-	SKREKK(1), ACTION(2), ROMANTISK(3), KOMEDIE(4), DOKUMENTAR(5), THRILLER(6), FANTASY(7);
+	SKREKK, ACTION, ROMANTISK, KOMEDIE, DOKUMENTAR, THRILLER, FANTASY;
 	
-	private int id;
-	private Sjanger (int id) {
-		this.id=id;
-	}
+	
 }
